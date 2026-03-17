53e849e2aff1daaf1605cd9c1879f9b370ff6864
Sarah Salah <158096323+Harsa1@users.noreply.github.com>
2026-03-17 16:12:23 -0400

    profil commencement

---
+++ Mobile/app/src/main/java/com/example/budgets/ProfilActivite.java
@@ -0,0 +1,17 @@
+package com.example.budgets;
+
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfilActivite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activite_connexion);
+    }
+
+}

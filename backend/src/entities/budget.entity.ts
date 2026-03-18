820ea3166e549b89768fbc84eddca803a7676068
PKatETS <panagiotis.katsanis.1@ens.etsmtl.ca>
2026-03-18 11:15:35 -0400

    fix

--- backend/src/entities/budget.entity.ts
+++ backend/src/entities/budget.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from './user.entity';
 
 @Entity()
 export class Budget {
@@ -9,4 +10,7 @@ export class Budget {
   solde: number;
 
   //FK pour enveloppe
+
+  @ManyToOne(() => User, (user) => user.budget)
+  user: User;
 }

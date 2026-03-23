029b7c5275ca7df466d1db2e9442a06b8c1f0342
annabellecard3nas <119866562+annabellecard3nas@users.noreply.github.com>
2026-03-22 20:57:43 -0400

    test

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { JwtService } from "@nestjs/jwt";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "src/entities";
 import { UserService } from "src/user/user.service";
+import { InscriptionDto } from "./dto";
 
 //bibliothèque officielle de NestJS pour gérer les JSON Web Tokens (JWT).
 //npm i --save @nestjs/jwt installer
@@ -14,4 +15,13 @@ export class AuthService{
     private readonly userService: UserService, 
     private readonly jwtService: JwtService
   ){}
+
+  // Ajoute cette fonction ici !
+    async inscription(dto: InscriptionDto) {
+        // C'est ici qu'on fera la logique plus tard
+        return {
+            message: "L'inscription a été reçue !",
+            data: dto
+        };
+    }
 }

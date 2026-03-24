8de52abd151e2b834edea767eeeb72c1d3aaa4b6
PKatETS <panagiotis.katsanis.1@ens.etsmtl.ca>
2026-03-24 11:22:01 -0400

    COntroller, DTO + 1/2 service enveloppe

--- backend/src/enveloppe/enveloppe.controller.ts
+++ backend/src/enveloppe/enveloppe.controller.ts
@@ -1,18 +1,38 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards } from "@nestjs/common";
 import { EnveloppeService } from "./enveloppe.service";
+import { CreateEnveloppeDto } from "./dto/update_enveloppe.dto copy";
+import { JwtAuthGuard } from "../auth/guard/jwt.guard";
+import { Request } from "@nestjs/common";
 
-
-// @Controller(), NestJS comprend : "C'est une adresse (URL) que 
-// mon app peut appeler
+@UseGuards(JwtAuthGuard)
 @Controller('enveloppe')
 export class EnveloppeController{
     
     constructor(private readonly enveloppeService: EnveloppeService) {}
 
-// Récupérer toutes les enveloppes
-  @Get() // GET http://localhost:3000/enveloppe
-    findAll() {
-        return this.enveloppeService.getAll();
+    @Post()
+    create(@Request() request, @Body() createEnveloppeDto: CreateEnveloppeDto) {
+        return this.enveloppeService.create(request.user.id, createEnveloppeDto);
     }
 
+    // Récupérer toutes les enveloppes
+    @Get()
+    findAll(@Request() request) {
+        return this.enveloppeService.findAll(request.user.id);
+    }
+
+    @Patch(':id')
+    update(@Request() request, @Body() updateEnveloppeDto: CreateEnveloppeDto, @Param('id', ParseIntPipe) id: number) {
+        return this.enveloppeService.update(request.user.id, updateEnveloppeDto, id);
+    }
+
+    @Get(':id')
+    findOne(@Request() request, @Param('id', ParseIntPipe) id: number) {
+        return this.enveloppeService.findOne(request.user.id, id);
+    }
+
+    @Delete(':id')
+    remove(@Request() request, @Param('id', ParseIntPipe) id: number) {
+        return this.enveloppeService.remove(request.user.id, id);
+    }
 }

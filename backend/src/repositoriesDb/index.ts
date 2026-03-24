f322ece20da274e987e1cd77970e1d25fbb39e14
annabellecard3nas <119866562+annabellecard3nas@users.noreply.github.com>
2026-03-24 11:50:53 -0400

    repositoriwDB commencer : User CRUD

---
+++ backend/src/repositoriesDb/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Action       Nom Normalisé      Description
+ * 
+ * Create       create(data)       Insérer une nouvelle entrée.
+ * Read (One)   get(id)            Récupérer une entrée unique par son ID.
+ * Read (All)   getAll(filters)    Récupérer une liste (avec pagination/filtres).
+ * Update       update(id, data)   Modifier une entrée existante.
+ * Delete       delete(id)         Supprimer une entrée.
+ * 
+ */

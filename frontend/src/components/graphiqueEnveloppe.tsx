0684cd43a599ca454009b9e54d2cad1513fbe14c
Justine Ouellette <jouellette0808@gmail.com>
2026-03-31 21:29:00 -0400

    graphique enveloppe terminé

--- frontend/src/components/graphiqueEnveloppe.tsx
+++ frontend/src/components/graphiqueEnveloppe.tsx
@@ -1,4 +1,6 @@
 import useDepense from "../hooks/UseDepense";
+// must install following librairy for chart
+// source is this library
 import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -29,34 +31,76 @@ export function GraphiqueEnveloppe() {
 
   if (depenses.length === 0)
     return <div>Vous n'avez présentement aucune dépense. </div>;
-  return (
-    <Bar
-      data={{
-        labels: [depenses.map((depense) => depense.date)],
-        datasets: [
-          {
-            label: "Dépenses",
-            data: depenses.map((depense) => depense.prix),
-            backgroundColor: "#96c16e",
-            borderColor: "#888",
-            borderWidth: 0.5,
+  else {
+    // un map initial clé-valeur pour les données de dépenses
+    const firstMap = new Map<string, number>();
+
+    // parcourir les dépenses et remplir le map avec la date et le prix total de la journée
+    depenses.forEach((depense) => {
+      // la date est en string au départ, convertir celle-ci en objet Date pour pouvoir la formater ensuite
+      const [year, month, day] = depense.date.split("-").map(Number);
+      const dateObj = new Date(year, month - 1, day);
+      const date = dateObj.toLocaleDateString("fr-CA"); // Format: YYYY-MM-DD
+      const daySpending = firstMap.get(date) || 0;
+      firstMap.set(date, daySpending + depense.prix);
+    });
+
+    // un map trié par la date, clé= date et valeur = prix total de la journée
+    const sortedDepenses = Array.from(firstMap.entries())
+      .map(([date, prix]) => ({ date, prix }))
+      .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+
+    return (
+      <Bar
+        data={{
+          labels: sortedDepenses.map((depense) => depense.date),
+          datasets: [
+            {
+              label: "Dépenses ($) ",
+
+              data: sortedDepenses.map((depense) => depense.prix),
+              backgroundColor: "#96c16e",
+              borderColor: "#888",
+              borderWidth: 0.5,
+            },
+          ],
+        }}
+        options={{
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            y: {
+              title: {
+                display: true,
+                text: "Prix (en $)",
+                color: "#333",
+                font: {
+                  size: 16,
+                  weight: "bold",
+                },
+              },
+              beginAtZero: true,
+              min: 0,
+              max:
+                Math.max(...sortedDepenses.map((depense) => depense.prix)) + 10,
+            },
+            x: {
+              title: {
+                display: true,
+                text: "7 derniers jours de dépenses",
+                color: "#333",
+                font: {
+                  size: 16,
+                  weight: "bold",
+                },
+              },
+            },
           },
-        ],
-      }}
-      options={{
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          y: {
-            beginAtZero: true,
-            min: 0,
-            max: Math.max(...depenses.map((depense) => depense.prix)) + 10,
-          },
-        },
-      }}
-      height={400}
-    />
-  );
+        }}
+        height={400}
+      />
+    );
+  }
 }
 
 export default GraphiqueEnveloppe;

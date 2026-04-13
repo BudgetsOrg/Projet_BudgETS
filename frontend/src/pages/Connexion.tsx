0c84d3a3aac2e7c1e808c278806c1f98dd2c45a5
MohamedAmineAllet <157835866+MohamedAmineAllet@users.noreply.github.com>
2026-04-13 11:14:10 -0400

    Nettoyage Connexion

--- frontend/src/pages/Connexion.tsx
+++ frontend/src/pages/Connexion.tsx
@@ -1,21 +1,12 @@
 // Mohamed
 import { useState } from "react";
 import { getToken, setToken } from "../../public/token";
-import type { Utilisateur } from "../interfaces";
 import { postConnexion } from "../api/UtilisateurApi";
 import img_retour from "../img/arrow_left_alt.png";
 import img_plante from "../img/image_inscription_plante.png";
 
 function Connexion() {
   const [erreur, setErreur] = useState("");
-  const [donneeConnexion, setDonneeConnexion] = useState<Utilisateur>({
-    adresse_email: "",
-    nom: "",
-    prenom: "",
-    password: "",
-    date_naissance: "",
-    soldeDumois: 0,
-  });
 
   const redirigerPageInscription = () => {
     window.location.href = "/PageInscription";
@@ -79,7 +70,7 @@ function Connexion() {
                 width: "458px",
                 textAlign: "center",
                 boxSizing: "border-box"
-              }}>
+              }} >
                 {erreur}
               </div>
             )}
@@ -117,68 +108,3 @@ function Connexion() {
   );
 }
 export default Connexion;
-/*import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-function Connexion() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-
-  const handleLogin = async () => {
-    const response = await fetch("http://localhost:3000/auth/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ adresse_email: email, password: password }),
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      localStorage.setItem("token", data.access_token);
-      navigate("/dashboard"); // redirect to main page after login
-    } else {
-      alert("Email ou mot de passe incorrect");
-    }
-  };
-  return (
-    <>
-      <div className="connexion_container">
-        <div className="container_gauche">
-          <img
-            src="/img/image_inscription_plante.png"
-            className="image_connexion"
-          />
-        </div>
-        <div className="connexion_information">
-          <h1>Connexion</h1>
-          <input
-            type="email"
-            name="btn_email"
-            placeholder="Adresse email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            name="btn_password"
-            placeholder="Mot de passe"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <label>Mot de passe oublié ?</label>
-          <button className="btn_connexion" type="submit" onClick={handleLogin}>
-            Connexion
-          </button>
-          <button className="btn_inscription" type="submit">
-            S'inscrire
-          </button>
-        </div>
-
-        <div className="triangle triangle-droite" />
-        <div className="triangle triangle-gauche" />
-      </div>
-    </>
-  );
-}
-export default Connexion;
-*/
